7d56ac03811debdfff84d0e5dfd67c7562fc1246
0-0-9 <bluemoon0407@163.com>
2016-07-25 17:50:10 +0800

    first commit

---
+++ apply-call/js/index.js
@@ -0,0 +1,17 @@
+var f1 = function(){
+  eval($("#apply1").text());
+ // alert(callSum1(5,6));
+  eval($("#apply2").text());
+};
+var f2 = function(){
+  eval($("#apply1").text());
+  alert(callSum2(5,6));
+};
+var f5 = function(){
+  eval($("#apply4").text());
+  eval($("#apply5").text());
+};
+var f6 = function(){
+  eval($("#apply4").text());
+  eval($("#apply6").text());
+};
