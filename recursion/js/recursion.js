22175f09a653f1e3f59d3a6c4ef26db7bbe47c02
0-0-9 <bluemoon0407@163.com>
2016-07-30 03:10:50 +0800

    add recursion

---
+++ recursion/js/recursion.js
@@ -0,0 +1,24 @@
+var af1 = function(){
+  eval($("#ap1").text());
+  eval($("#ab1").text());
+};
+var af2 = function(){
+  eval($("#ap1").text());
+  eval($("#ab2").text());
+};
+var af3 = function(){
+  eval($("#ap2").text());
+  eval($("#ab3").text());
+};
+var af4 = function(){
+  eval($("#ap2").text());
+  eval($("#ab4").text());
+};
+var af5 = function(){
+  eval($("#ap3").text());
+  eval($("#ab5").text());
+};
+var af6 = function(){
+  eval($("#ap3").text());
+  eval($("#ab6").text());
+};
