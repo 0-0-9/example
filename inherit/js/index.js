e16b99c8fbef5095220b4e34ec559dc74e31d37f
0-0-9 <bluemoon0407@163.com>
2016-07-30 01:54:23 +0800

    alter inherit

---
+++ inherit/js/index.js
@@ -0,0 +1,64 @@
+var af1 = function(){
+    eval($("#ap1").text());
+    eval($("#ab1").text());
+};
+var af2 = function(){
+    eval($("#ap1").text());
+    eval($("#ab2").text());
+};
+var af3 = function(){
+  eval($("#ap2").text());
+  eval($("#ab3").text());
+};
+var af4 = function(){
+  eval($("#ap2").text());
+  eval($("#ab4").text());
+};
+var bf1 = function(){
+  eval($("#bp1").text());
+  eval($("#bb1").text());
+};
+var bf2 = function(){
+  eval($("#bp1").text());
+  eval($("#bb2").text());
+};
+var bf3 = function(){
+  eval($("#bp2").text());
+  eval($("#bb3").text());
+};
+var bf4 = function(){
+  eval($("#bp2").text());
+  eval($("#bb4").text());
+};
+var cf1 = function(){
+  eval($("#cp1").text());
+  eval($("#cb1").text());
+};
+var cf2 = function(){
+  eval($("#cp1").text());
+  eval($("#cb2").text());
+};
+var cf3 = function(){
+  eval($("#cp1").text());
+  eval($("#cb3").text());
+};
+var cf4 = function(){
+  eval($("#cp1").text());
+  eval($("#cb4").text());
+};
+var cf5 = function(){
+  eval($("#cp1").text());
+  eval($("#cb5").text());
+};
+var cf6 = function(){
+  eval($("#cp1").text());
+  eval($("#cb6").text());
+};
+var cf7 = function(){
+  eval($("#cp1").text());
+  eval($("#cb7").text());
+};
+var cf8 = function(){
+  eval($("#cp1").text());
+  eval($("#cb8").text());
+};
