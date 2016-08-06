b3bcc8f6b67cc719ce8b9e0eaedc25c5818fdea3
0-0-9 <bluemoon0407@163.com>
2016-08-07 02:31:53 +0800

    alter location

--- location/js/location.js
+++ location/js/location.js
@@ -5,3 +5,24 @@ var af1 = function(){
 var tf1 = function(){
   eval($("#tb1").text());
 };
+var tf2 = function(){
+  eval($("#tb2").text());
+};
+var tf3 = function(){
+  eval($("#tb3").text());
+};
+var tf4 = function(){
+  eval($("#tb4").text());
+};
+var tf5 = function(){
+  eval($("#tb5").text());
+};
+var tf6 = function(){
+  eval($("#tb6").text());
+};
+var tf7 = function(){
+  eval($("#tb7").text());
+};
+var tf8 = function(){
+  eval($("#tb8").text());
+};
