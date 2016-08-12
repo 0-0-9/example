96fa0c573f76da9b3abde402fb5c8add95400792
0-0-9 <bluemoon0407@163.com>
2016-08-13 02:17:00 +0800

    add nodeType

--- nodeType/js/nodeType.js
+++ nodeType/js/nodeType.js
@@ -1,4 +1,7 @@
 
+window.onload = function(){
+
+};
 var af1 = function(){
   eval($("#ap1").text());
 };
@@ -36,54 +39,74 @@ var bf6 = function(){
     eval($("#bb6").text());
 };
 var div = document.getElementById("docDivId");
-// div.id = "docDivId";
-// div.className = "docDiv";
-// div.title = "this is title";
-// div.lang = "en";
-// div.dir = "ltr";
+function changeToDocDivId() {
+    if (document.getElementById("docDivId2")) {
+        // var div = document.getElementById("docDivId2");
+        div.id = "docDivId";
+        div.className = "docDiv";
+        div.title = "this is title";
+        div.lang = "en";
+        div.dir = "ltr";
+    }
+}
+function changeToDocDivId2() {
+    if (document.getElementById("docDivId")) {
+        // var div = document.getElementById("docDivId");
+        div.id = "docDivId2";
+        div.className = "docDiv2";
+        div.title = "this is title2";
+        div.lang = "fr";
+        div.dir = "rtl";
+    }
+}
 var cf1 = function(){
+    changeToDocDivId();
   // eval($("#cp2").text());
   eval($("#cb1").text());
 };
 var cf2 = function(){
   // eval($("#cp2").text());
+  changeToDocDivId();
   eval($("#cb2").text());
 };
 var cf3 = function(){
+    changeToDocDivId();
   // eval($("#cp2").text());
   eval($("#cb3").text());
 };
 var cf4 = function(){
+    changeToDocDivId();
   // eval($("#cp2").text());
   eval($("#cb4").text());
 };
 var cf5 = function(){
+    changeToDocDivId();
   // eval($("#cp2").text());
   eval($("#cb5").text());
 };
 
-div.id = "docDivId2";
-div.className = "docDiv2";
-div.title = "this is title2";
-div.lang = "fr";
-div.dir = "rtl";
 var cf6 = function(){
+    changeToDocDivId2();
   // eval($("#cp3").text());
   eval($("#cb6").text());
 };
 var cf7 = function(){
+    changeToDocDivId2();
   // eval($("#cp3").text());
   eval($("#cb7").text());
 };
 var cf8 = function(){
+    changeToDocDivId2();
   // eval($("#cp3").text());
   eval($("#cb8").text());
 };
 var cf9 = function(){
+    changeToDocDivId2();
   // eval($("#cp3").text());
   eval($("#cb9").text());
 };
 var cf10 = function(){
+    changeToDocDivId2();
   // eval($("#cp3").text());
   eval($("#cb10").text());
 };
