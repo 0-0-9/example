ac2ecf324d9a9d932794a94c6ec294eec4d9d6d9
0-0-9 <bluemoon0407@163.com>
2016-08-13 02:02:01 +0800

    add nodeType

---
+++ nodeType/js/nodeType.js
@@ -0,0 +1,89 @@
+
+var af1 = function(){
+  eval($("#ap1").text());
+};
+var af2 = function(){
+  eval($("#ap2").text());
+};
+var af3 = function(){
+  eval($("#ap3").text());
+};
+var af4 = function(){
+  eval($("#ap4").text());
+};
+var bf1 = function(){
+    eval($("#bp2").text());
+    eval($("#bb1").text());
+};
+var bf2 = function(){
+    eval($("#bp2").text());
+    eval($("#bb2").text());
+};
+var bf3 = function(){
+    eval($("#bp2").text());
+    eval($("#bb3").text());
+};
+var bf4 = function(){
+    eval($("#bp2").text());
+    eval($("#bb4").text());
+};
+var bf5 = function(){
+    eval($("#bp2").text());
+    eval($("#bb5").text());
+};
+var bf6 = function(){
+    eval($("#bp2").text());
+    eval($("#bb6").text());
+};
+var div = document.getElementById("docDivId");
+// div.id = "docDivId";
+// div.className = "docDiv";
+// div.title = "this is title";
+// div.lang = "en";
+// div.dir = "ltr";
+var cf1 = function(){
+  // eval($("#cp2").text());
+  eval($("#cb1").text());
+};
+var cf2 = function(){
+  // eval($("#cp2").text());
+  eval($("#cb2").text());
+};
+var cf3 = function(){
+  // eval($("#cp2").text());
+  eval($("#cb3").text());
+};
+var cf4 = function(){
+  // eval($("#cp2").text());
+  eval($("#cb4").text());
+};
+var cf5 = function(){
+  // eval($("#cp2").text());
+  eval($("#cb5").text());
+};
+
+div.id = "docDivId2";
+div.className = "docDiv2";
+div.title = "this is title2";
+div.lang = "fr";
+div.dir = "rtl";
+var cf6 = function(){
+  // eval($("#cp3").text());
+  eval($("#cb6").text());
+};
+var cf7 = function(){
+  // eval($("#cp3").text());
+  eval($("#cb7").text());
+};
+var cf8 = function(){
+  // eval($("#cp3").text());
+  eval($("#cb8").text());
+};
+var cf9 = function(){
+  // eval($("#cp3").text());
+  eval($("#cb9").text());
+};
+var cf10 = function(){
+  // eval($("#cp3").text());
+  eval($("#cb10").text());
+};
