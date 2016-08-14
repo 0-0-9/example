05779786588b5478027ae2f35a000e395b00d6a2
0-0-9 <bluemoon0407@163.com>
2016-08-14 18:04:26 +0800

    alter event

--- event/js/event.js
+++ event/js/event.js
@@ -28,8 +28,50 @@ var af4 = function(){
 };
 var btn5 = document.getElementById("ab5");
 var showThis = function(){
-    alert("this === window");
+    alert(this === window);
 }
 if (btn5.attachEvent) {
-    btn5.attachEvent("onclick",showId);
+    btn5.attachEvent("onclick",showThis);
+};
+
+  eval($("#ap7").text());
+var EventUtil = {
+    addHandler: function(ele, type, handler) {
+        if (ele.addEventListener) {
+            ele.addEventListener(type, handler, false);
+        } else if (ele.attachEvent) {
+            ele.attachEvent("on" + type, handler);
+        } else {
+            ele["on" + type] = handler;
+        }
+    },
+    getEvent: function(event) {
+        return event ? event : window.event;
+    },
+    getTarget: function(event) {
+        return event.target || event.srcElement;
+    },
+    preventDefault: function(event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+    removeHandler: function(ele, type, handler) {
+        if (ele.removeEventListener) {
+            ele.removeEventListener(type, handler, false);
+        } else if (ele.detachEvent) {
+            ele.detachEvent("on" + type, handler);
+        } else {
+            ele["on" + type] = null;
+        }
+    },
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    }
 };
