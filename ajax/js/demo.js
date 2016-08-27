d6df1bc1b9aea91301590f36b32b4fce48dc95fa
0-0-9 <bluemoon0407@163.com>
2016-08-27 21:30:41 +0800

    d

---
+++ ajax/js/demo.js
@@ -0,0 +1,33 @@
+var EventUtil = {
+    addHandler: function(ele, type, handler) {
+        if (ele.addEventListener) {
+            ele.addEventListener(type, handler, false);
+        } else if (ele.attachEvent) {
+            ele.attachEvent("on" + type, handler);
+        } else {
+            ele["on" + type] = handler;
+        }
+    },
+    removeHandler: function(ele, type, handler) {
+        if (ele.removeEventListener) {
+            ele.removeEventListener(type, handler, false);
+        } else if (ele.detachEvent) {
+            ele.detachEvent("on" + type, handler);
+        } else {
+            ele["on" + type] = null;
+        }
+    }
+};
+
+var searchFn = function() {
+    var keywordVal = document.getElementById("keyword").value;
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://php-test-lanyy.c9users.io/server.php?number=" + keywordVal);
+    request.send();
+    // alert(keywordVal);
+};
+
+window.onload = function() {
+    var searchBtn = document.getElementById("searchBtn");
+    EventUtil.addHandler(searchBtn, "click", searchFn);
+};
