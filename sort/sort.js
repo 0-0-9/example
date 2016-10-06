a92d243c9f3bba349991b990e412540b43802410
0-0-9 <bluemoon0407@163.com>
2016-10-06 13:52:23 +0800

    [add] sort examples

---
+++ sort/sort.js
@@ -0,0 +1,23 @@
+var af1 = function() {
+    eval($("#ap1").text());
+    eval($("#ab1").text());
+};
+var af2 = function() {
+    eval($("#ap2").text());
+    eval($("#ab2").text());
+};
+var af3 = function() {
+    eval($("#ap3").text());
+    eval($("#ab3").text());
+};
+var df1 = function() {
+    eval($("#dp1").text());
+    eval($("#db1").text());
+};
+var ef1 = function() {
+    eval($("#ep1").text());
+    eval($("#eb1").text());
+};
+var arr = [4, 24, 13, 6, 2, 9, 31];
+
+// console.log(mergeSort(arr));
